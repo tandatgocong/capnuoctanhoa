4592b0d65b5a24474d03e3c35529b89c2f0f81df
oanhle1508 <oanhle1508@b8ee3365-071f-3f57-fc0c-24652f504cdd>
2013-07-10 06:15:57 +0000

    In danh sách theo kỳ

--- trunk/CAPNUOCTANHOA/DAL/QLDHN/C_HoaDon_0.cs
+++ trunk/CAPNUOCTANHOA/DAL/QLDHN/C_HoaDon_0.cs
@@ -15,7 +15,7 @@ namespace CAPNUOCTANHOA.DAL.QLDHN
 
         public static DataTable getBangKeBaoThay(int sobangke)
         {
-            string sql = "SELECT GHD.ID,DHN_SOBANGKE, DHN_DANHBO,DHN_HUYCAMKET,DHN_CAMKET,DHN_BAMHI,DHN_GHICHU,HOTEN, SONHA + ' ' +TENDUONG AS 'DIACHI',CODH,SOTHANDH,DHN_DOT,DHN_KY,DHN_NAM,DHN_NGAYGHINHAN,DHN_TODS,LOTRINH";
+            string sql = "SELECT GHD.ID,DHN_SOBANGKE, DHN_DANHBO,DHN_HUYCAMKET,DHN_CAMKET,DHN_BAMHI,DHN_GANMOI,DHN_GHICHU,HOTEN, SONHA + ' ' +TENDUONG AS 'DIACHI',CODH,SOTHANDH,DHN_DOT,DHN_KY,DHN_NAM,DHN_NGAYGHINHAN,DHN_TODS,LOTRINH";
             sql += " FROM DK_GIAMHOADON GHD,TB_DULIEUKHACHHANG kh WHERE  kh.DANHBO=GHD.DHN_DANHBO AND DHN_SOBANGKE='" + sobangke + "' ORDER BY DHN_DANHBO ASC ";
             return LinQConnection.getDataTable(sql);
         }
@@ -55,7 +55,13 @@ namespace CAPNUOCTANHOA.DAL.QLDHN
             sql += " FROM DK_GIAMHOADON GHD,TB_DULIEUKHACHHANG kh WHERE (DHN_HUYCAMKET <>'' or DHN_HUYCAMKET is not null) AND kh.DANHBO=GHD.DHN_DANHBO AND DHN_SOBANGKE='" + sobangke + "' ORDER BY DHN_DANHBO ASC ";
             return LinQConnection.getDataTable(sql);
         }
-        public static int UpdateSoBangKe(string danhbo,string sobangke,string ngayghinhan,string nam,string tods,string ghichu,string bamchi,int ky,string dot,string chisoky,string huycamket)
+        public static DataTable getBangKeBaoThayGanMoi(int sobangke)
+        {
+            string sql = "SELECT DHN_SOBANGKE, DHN_DANHBO,HOTEN, SONHA + ' ' +TENDUONG AS 'DIACHI',CODH,SOTHANDH,DHN_DOT,DHN_KY,DHN_NAM,DHN_NGAYGHINHAN,DHN_BAMHI,DHN_TODS,DHN_GHICHU,LOTRINH,DHN_GANMOI";
+            sql += " FROM DK_GIAMHOADON GHD,TB_DULIEUKHACHHANG kh WHERE (DHN_GANMOI <>'' or DHN_GANMOI is not null) AND kh.DANHBO=GHD.DHN_DANHBO AND DHN_SOBANGKE='" + sobangke + "' ORDER BY DHN_DANHBO ASC ";
+            return LinQConnection.getDataTable(sql);
+        }
+        public static int UpdateSoBangKe(string danhbo,string sobangke,string ngayghinhan,string nam,string tods,string ghichu,string bamchi,int ky,string dot,string chisoky,string huycamket,string ganmoi)
         {
             int kq = 0;
             string sql = "UPDATE DK_GIAMHOADON set DHN_NGAYGHINHAN='" + ngayghinhan + "',DHN_NAM=" + nam + ",DHN_TODS='" + tods + "',DHN_GHICHU=N'" + ghichu + "',DHN_DOT='" + dot + "',DHN_KY='"+chisoky +"'";
@@ -82,6 +88,15 @@ namespace CAPNUOCTANHOA.DAL.QLDHN
             {
                 sql = sql + ",DHN_HUYCAMKET=null";
             }
+            if (ganmoi == "X")
+            {
+                sql = sql + ",DHN_GANMOI='" + ganmoi +"'";
+            }
+            else if (ganmoi != "X")
+            {
+                sql = sql + ",DHN_GANMOI=null";
+            }
+
             sql = sql + ",DHN_MODIFYDATE=getDate(),DHN_MODIFYBY='"+DAL.SYS.C_USERS._userName+"' where DHN_DANHBO='" + danhbo + "' and DHN_SOBANGKE='" + sobangke + "'";
             kq = LinQConnection.ExecuteCommand_(sql);
             return kq;
